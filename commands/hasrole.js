81954871820a49b20cd787b36b51da5a7a94c6f0
Kamal Ahsan <52078511+KamalAhsan03@users.noreply.github.com>
2022-01-20 13:43:44 -0500

    Update hasrole.js

--- commands/hasrole.js
+++ commands/hasrole.js
@@ -23,7 +23,7 @@ module.exports = {
         })
 
         if (!role) {
-            msg.reply(`${roleName} role doesn't exist g. :gun: `)
+            msg.reply(`${roleName} role doesn't exist. :gun: `)
             return;
         }
 
@@ -38,7 +38,7 @@ module.exports = {
             }
         } else {
             msg.react('\🔫')
-            msg.reply("This is treason, can't snake roles goon :angry: :gun:");
+            msg.reply("This is treason, can't snake roles :angry: :gun:");
         }
 
 
