68172239f825b942ac0276e650f5e3b2cac3832b
Kamal Ahsan <52078511+KamalAhsan03@users.noreply.github.com>
2022-01-20 13:42:09 -0500

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,11 +2,11 @@ module.exports = {
     name: 'clear',
     descrpition: 'Clear Command!',
     async execute(msg, args) {
-        if (!args[0]) return msg.reply('Specify how many insider deals you want to finesse cuz :100: :pray: ');
-        if (isNaN(args[0])) return msg.reply('Allow numbers in numerals g :pray: :gun:');
+        if (!args[0]) return msg.reply('Specify how many insider deals you want to finesse :100: :pray: ');
+        if (isNaN(args[0])) return msg.reply('Allow numbers in numerals :pray: :gun:');
 
-        if (args[0] > 100) return msg.reply('The homie tryna delete all the secrets chill with the treason use less than 100 cuz :pray: :gun: :crossed_swords:');
-        if (args[0] < 1) return msg.reply('Gotta delete some secrets homes :gun: :pray: ');
+        if (args[0] > 100) return msg.reply('The homie tryna delete all the secrets chill with the treason use less than 100 :pray: :gun: :crossed_swords:');
+        if (args[0] < 1) return msg.reply('Gotta delete some secrets :gun: :pray: ');
 
         await msg.channel.messages.fetch({
             limit: args[0]
